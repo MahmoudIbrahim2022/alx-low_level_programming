3a907321ff1548a53c57a62bbaaf3dd0d6e5b4f7
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-06-15 10:11:14 -0700

    Fix 4-puts.c File

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){    
-char str[52];
-strcpy(str,"\"Programming is like building a multilingual puzzle");
-puts(str);
-    
-return 0;   
+/**
+ * main - Entry point
+*/
+int main(void)
+{    
+	char str[52];
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
+	puts(str);    
+	return (0);   
 }
