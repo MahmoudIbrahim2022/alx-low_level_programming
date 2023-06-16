93c93ff678b698f8701da43fa2d91d982715adfb
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-06-16 07:39:26 -0700

    Add 100-print_comb3.c File

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:  0 (Success)
+*/
+int main(void)
+{
+	int n1;
+	int n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{	
+	for (n2 = n1 + 1; n2 < 10; n2++)
+		{
+		if (n1 == n2)
+		continue;
+		putchar('0' + n1);
+		putchar('0' + n2);
+		if (n1 < 8)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
