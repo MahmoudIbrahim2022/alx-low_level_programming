94eca382033d925dfd63523a790fab3240303d9e
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-06-20 15:54:28 -0700

    Fix 102-fibonacci.c File

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i;
-	int array[50];
+	long array[50];
 
 	array[0] = 1;
 	array[1] = 2;
@@ -16,11 +16,13 @@ int main(void)
 	for (i = 2; i < 50; i++)
 	{
 		array[i] = array[i - 1] + array[i - 2];
-		if (i == 49)
-		printf("%d\n", array[i]);
-		else
-		printf("%d, ", array[i]);
 	}
-
+	for (i = 0; i < 50; i++)
+	{
+		if (i == 49)
+		printf("%lu\n", array[i]);
+		else
+		printf("%lu, ", array[i]);
+	}
 	return (0);
 }
