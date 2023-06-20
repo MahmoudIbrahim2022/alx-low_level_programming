24e58a6e3d840d535d3afb35e3da0ae05583f42b
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-06-20 15:45:01 -0700

    Add 102-fibonacci.c File

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,12 +9,13 @@ int main(void)
 {
 	int i;
 	int array[50];
+
 	array[0] = 1;
 	array[1] = 2;
 
 	for (i = 2; i < 50; i++)
 	{
-		array[i] = array[i-1] + array[i-2];
+		array[i] = array[i - 1] + array[i - 2];
 		if (i == 49)
 		printf("%d\n", array[i]);
 		else
