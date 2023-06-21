6be509f38b1a3a1cc6f8a523e9afcd1b6cff1220
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-06-21 05:31:11 -0700

    Fix 0-main.c File

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,8 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-int add(int, int);
-void print_to_98(int n);
-int postitive_or_negative(int);
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
