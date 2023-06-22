93647ff34dbf2720f144c8ffa6d3a7513604b46f
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-06-22 08:42:20 -0700

    Fix 101-print_number.c File

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * print_positive_number - prints a positive integer
+ * @n : Integer to Print
+ *
+ * Return:  void
+ *
+*/
+
+void print_positive_number(int n)
+{
+
+	int digit;
+
+	if (n == 0)
+	return;
+
+
+	digit = n % 10;
+	print_positive_number(n / 10);
+	_putchar('0' + digit);
+}
+
+/**
  * print_number - prints an integer
  * @n : Integer to Print
  *
@@ -10,14 +32,18 @@
 
 void print_number(int n)
 {
-	int digit;
 
 	if (n == 0)
-	return;
+	_putchar(48);
+	else if (n < 0)
+	{
+		n *= -1;
+		_putchar(45);
+		print_positive_number(n);
+	}
+	else
+	print_positive_number(n);
 
 
-	digit = n % 10;
-	print_number(n / 10);
-	_putchar('0' + digit);
 
 }
