92b0f1a864695054b2af65c5546fcfb53493bc79
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-06-22 09:11:57 -0700

    Add 100-prime_factor.c File

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return:  0 (Success)
+*/
+int main(void)
+{
+	long int n = 612852475143;
+	long int div = 2, ans = 0, maxFact;
+
+	while (n != 0)
+	{
+		if (n % div != 0)
+		div = div + 1;
+		else
+		{
+			maxFact = n;
+			n = n / div;
+			if (n == 1)
+			{
+				printf("%li\n", maxFact);
+				ans = 1;
+				break;
+			}
+		}
+	}
+	return (0);
+}
