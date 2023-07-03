f616aa24721ae164f53eb2ffda5da03eb0fcd7f0
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-07-03 05:01:33 -0700

    Fix 2-strchr.c File

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "main.h"
 
 /**
@@ -13,7 +14,7 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	char *p;
+	char *p = NULL;
 
 	while (s[i] != '\0')
 	{
@@ -22,6 +23,7 @@ char *_strchr(char *s, char c)
 			p = (s + i);
 			break;
 		}
+
 		i++;
 	}
 
