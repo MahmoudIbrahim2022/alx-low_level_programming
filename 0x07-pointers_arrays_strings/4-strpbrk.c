35e0174dd96f1ff2d94fa50495149085ab91408b
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-07-03 07:47:49 -0700

    Fix 4-strpbrk.c File

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "main.h"
 
 /**
@@ -18,8 +19,10 @@ char *_strpbrk(char *s, char *accept)
 	while ((c = *s++) != 0)
 	{
 		for (scanp = accept; (sc = *scanp++) != 0;)
-		if (sc == c)
-		return ((char *)(s - 1));
+		{
+			if (sc == c)
+			return ((char *)(s - 1));
+		}
 	}
 
 	return (NULL);
