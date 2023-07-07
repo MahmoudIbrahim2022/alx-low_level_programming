f9a31214ec3a34eb85be92422d0d456e1b248068
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-07-07 13:03:49 -0700

    Add 0-whatsmyname.c File

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+
+
 
 /**
  * main - prints its name, followed by a new line.
@@ -7,17 +10,10 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i = 0;
 
-	while (*(*(argv) + i) != '\0')
-	{
-		_putchar(*(*(argv) + i));
-		i++;
-	}
-
-	_putchar('\n');
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
