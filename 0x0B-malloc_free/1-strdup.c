26a7766f7ac96643f94aaef9548584b9fd72e4fe
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-07-11 08:26:48 -0700

    Fix 1-strdup.c File

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,12 +16,16 @@ char *_strdup(char *str)
 	int size = 0;
 	char *s;
 
+	if (str == NULL)
+	return (0);
+	else
 	while (str[size] != '\0')
+	size++;
 	size++;
 
 	s = (char *) malloc(size * sizeof(char));
 
-	if (size == 0 || s == 0)
+	if (s == 0)
 	return (0);
 	else
 	{
@@ -29,5 +33,6 @@ char *_strdup(char *str)
 		s[i] = str[i];
 		return (s);
 	}
+
 
 }
