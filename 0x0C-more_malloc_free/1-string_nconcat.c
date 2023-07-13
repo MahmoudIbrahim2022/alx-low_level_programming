72ba5b3ebe71faa354e49806f7f1fdbcb9d103bc
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-07-13 06:23:48 -0700

    Fix 1-string_nconcat.c File

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,9 +37,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s[i] = s1[i];
 
 	for (j = 0; j < n; j++)
-	s[i + j] = s2[j];
+	{
+		s[i] = s2[j];
+		i++;
+	}
 
-	s[i + j] = '\0';
+	s[i] = '\0';
 
 	return (s);
 
