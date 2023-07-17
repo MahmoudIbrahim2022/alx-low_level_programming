a706e3af901c2afc9ea67d4aaea0303a6410a375
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-07-17 13:19:37 -0700

    Add dog.h File

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+# C - Structures, typedef
+This project is for Structures, typedef in C programming Language
+
+## Contents
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog data type
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: dog data type
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
