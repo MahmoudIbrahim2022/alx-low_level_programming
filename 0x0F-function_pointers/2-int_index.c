b33363e40103b0cca35001055a3ecbb2025a74ef
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-07-19 09:15:50 -0700

    Add 2-int_index.c File

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+/**
+ * array_iterator - searches for an integer.
+ * @array: array in which integer will be searched
+ * @size: array size
+ * @cmp:  The function to be used to compare values.
+ *
+ * Return: index for the searched integer.
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int result;
+
+	if (size <= 0)
+	return (-1);
+
+	if (array && cmp)
+	for (i = 0; i < size; i++)
+	{
+		result = (*cmp)(array[i]);
+		if ( result != 0)
+		return (i);
+	}
+
+	return (-1);
+
+}
