ffb6e68dd777782ccbafdc31ebbce9cf91cbf47f
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-07-19 12:34:41 -0700

    Add Calc Files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,80 @@
+/**
+ * op_add - add Two Integers .
+ * @a: first Integer
+ * @b: second Integer
+ *
+ * Return: the sum of a and b.
+ *
+ */
+
+int op_add(int a, int b)
+{
+
+	return (a + b);
+
+}
+
+
+/**
+ * op_sub - subtract Two Integers .
+ * @a: first Integer
+ * @b: second Integer
+ *
+ * Return: the difference of a and b.
+ *
+ */
+
+int op_sub(int a, int b)
+{
+
+	return (a - b);
+
+}
+
+/**
+ * op_mul - multiply Two Integers .
+ * @a: first Integer
+ * @b: second Integer
+ *
+ * Return: the product of a and b.
+ *
+ */
+
+int op_mul(int a, int b)
+{
+
+	return (a * b);
+
+}
+
+/**
+ * op_div - divide Two Integers .
+ * @a: first Integer
+ * @b: second Integer
+ *
+ * Return: the division of a by b.
+ *
+ */
+
+int op_div(int a, int b)
+{
+
+	return (a / b);
+
+}
+
+/**
+ * op_mod - get the remainder of division of Two Integers .
+ * @a: first Integer
+ * @b: second Integer
+ *
+ * Return: the remainder of the division of a by b.
+ *
+ */
+
+int op_mod(int a, int b)
+{
+
+	return (a % b);
+
+}
