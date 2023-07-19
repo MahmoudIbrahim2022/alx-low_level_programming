b12897ea706b0bfa7c01bbf564df3827a99f8091
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-07-19 08:39:43 -0700

    Add 0-print_name.c File

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+
+/**
+ * print_name - prints a name.
+ * @name: The name to print
+ * @f: function that will print the name.
+ *
+ * Return: void.
+ *
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	(*f)(name);
+
+}
