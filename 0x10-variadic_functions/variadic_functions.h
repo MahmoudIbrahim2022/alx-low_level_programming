417a353939a7b84bf22090afa15e7fbeb199a68e
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-07-20 13:35:43 -0700

    Fix 2-print_strings.c File

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,23 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/**
+ * struct token - struct token
+ *
+ * @token: format token
+ * @f: The function associated
+ */
+
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
