5f6cc6cf22145df769fa5bd87713da805984d761
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-08-03 15:49:39 -0700

    Fix 5-flip_bits.c Files

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,12 +2,12 @@
 
 
 /**
- * flip_bits - returns the number of bits you would need 
+ * flip_bits - returns the number of bits you would need
  * to flip to get from one number to another.
  * @n: First Number
  * @m: Second index
  *
- * Return: returns the number of bits you would need 
+ * Return: returns the number of bits you would need
  * to flip to get from one number to another.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
