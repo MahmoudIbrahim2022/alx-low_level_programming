69159d6ab2344cc3fd710d7784d39decd3e2b712
MahmoudIbrahim2022 <mahmoud.ibrahim.1991@gmail.com>
2023-08-07 15:12:41 -0700

    Fix 3-cp.c File

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,7 @@
 
 #define USAGE "cp file_from file_to\n"
 #define ERR_NOREAD "ERROR: Can't read from file %s\n"
-#define ERR_NOWRITE "ERROR: Can't write to file %s\n"
+#define ERR_NOWRITE "ERROR: Can't write to %s\n"
 #define ERR_NOCLOSE "ERROR: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
